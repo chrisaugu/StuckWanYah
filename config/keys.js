2a97961bc46eaf5614fc6a3b823f525ab26cde1c
Christian Augustyn <30349879+chrisaugu@users.noreply.github.com>
2022-07-13 22:16:04 +1000

    small fixes

--- config/keys.js
+++ config/keys.js
@@ -13,7 +13,7 @@ module.exports = {
 		
 		callbackURL: process.env.FACEBOOK_CALLBACK_URL,
 		profileURL: 'https://graph.facebook.com/v2.5/me?fields=id,name,first_name,last_name,age_range,birthday,gender,picture{url},friends{id}',
-		profileFields: ['id','displayName','name','gender','age_range','birthday','link','picture.width(720).height(720) ','friends'],
+		profileFields: ['id','displayName','name','gender','age_range','birthday','link','picture.width(720).height(720)','friends'],
 	
 		// Memorable word to verify webhooks with Messenger platform
 		botVerifyToken: process.env.BOT_VERIFICATION_TOKEN
