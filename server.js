f21a7689049c87ef567fe2af8f39a63ffef54612
Christian Augustyn <30349879+chrisaugu@users.noreply.github.com>
2022-07-17 22:24:57 +1000

    fix: signature not defined

--- server.js
+++ server.js
@@ -3121,7 +3121,8 @@ router.get("/webhook", (req, res) => {
       // Respond with the challenge token from the request
       console.log("WEBHOOK_VERIFIED");
       res.status(200).send(challenge);
-    } else {
+    }
+    else {
       // Respond with '403 Forbidden' if verify tokens do not match
       res.sendStatus(403);
     }
@@ -3130,119 +3131,130 @@ router.get("/webhook", (req, res) => {
 
 // Create the endpoint for your webhook
 router.post("/webhook", (req, res) => {
-  let body = req.body;
+	let body = req.body;
 
-  console.log(`\u{1F7EA} Received webhook:`);
-  console.dir(body, { depth: null });
+	console.log(`\u{1F7EA} Received webhook:`);
+	console.dir(body, { depth: null });
 
-  // Check if this is an event from a page subscription
-  if (body.object === "page") {
-    // Returns a '200 OK' response to all requests
-    res.status(200).send("EVENT_RECEIVED");
+	// Check if this is an event from a page subscription
+	if (body.object === "page") {
 
-    // Iterate over each entry - there may be multiple if batched
-    body.entry.forEach(async function(entry) {
-      if ("changes" in entry) {
-        // Handle Page Changes event
-        let receiveMessage = new Receive();
-        if (entry.changes[0].field === "feed") {
-          let change = entry.changes[0].value;
-          switch (change.item) {
-            case "post":
-              return receiveMessage.handlePrivateReply(
-                "post_id",
-                change.post_id
-              );
-            case "comment":
-              return receiveMessage.handlePrivateReply(
-                "comment_id",
-                change.comment_id
-              );
-            default:
-              console.warn("Unsupported feed change type.");
-              return;
-          }
-        }
-      }
+		// Iterate over each entry - there may be multiple if batched
+		body.entry.forEach(async function(entry) {
+			// if ("changes" in entry) {
+			//     // Handle Page Changes event
+			//     let receiveMessage = new Receive();
+			//     if (entry.changes[0].field === "feed") {
+			//       let change = entry.changes[0].value;
+			//       switch (change.item) {
+			//         case "post":
+			//           return receiveMessage.handlePrivateReply(
+			//             "post_id",
+			//             change.post_id
+			//           );
+			//         case "comment":
+			//           return receiveMessage.handlePrivateReply(
+			//             "comment_id",
+			//             change.comment_id
+			//           );
+			//         default:
+			//           console.warn("Unsupported feed change type.");
+			//           return;
+			//       }
+			//     }
+			// }
 
-      // Iterate over webhook events - there may be multiple
-      entry.messaging.forEach(async function(webhookEvent) {
-        // Discard uninteresting events
-        if ("read" in webhookEvent) {
-          console.log("Got a read event");
-          return;
-        } else if ("delivery" in webhookEvent) {
-          console.log("Got a delivery event");
-          return;
-        } else if (webhookEvent.message && webhookEvent.message.is_echo) {
-          console.log(
-            "Got an echo of our send, mid = " + webhookEvent.message.mid
-          );
-          return;
-        }
+			// Get the webhook event. entry.messaging is an array, but 
+			// will only ever contain one event, so we get index 0
+			let webhook_event = entry.messaging[0];
+			console.log(webhook_event);
 
-        // Get the sender PSID
-        let senderPsid = webhookEvent.sender.id;
-        // Get the user_ref if from Chat plugin logged in user
-        let user_ref = webhookEvent.sender.user_ref;
-        // Check if user is guest from Chat plugin guest user
-        let guestUser = isGuestUser(webhookEvent);
+			// Get the sender PSID
+			let sender_psid = webhook_event.sender.id;
+			console.log('Sender PSID: ' + sender_psid);
 
-        if (senderPsid != null && senderPsid != undefined) {
-          if (!(senderPsid in users)) {
-            if (!guestUser) {
-              // Make call to UserProfile API only if user is not guest
-              let user = new User(senderPsid);
-              GraphApi.getUserProfile(senderPsid)
-                .then(userProfile => {
-                  user.setProfile(userProfile);
-                })
-                .catch(error => {
-                  // The profile is unavailable
-                  console.log(JSON.stringify(body));
-                  console.log("Profile is unavailable:", error);
-                })
-                .finally(() => {
-                  console.log("locale: " + user.locale);
-                  users[senderPsid] = user;
-                  i18n.setLocale("en_US");
-                  console.log(
-                    "New Profile PSID:",
-                    senderPsid,
-                    "with locale:",
-                    i18n.getLocale()
-                  );
-                  return receiveAndReturn(
-                    users[senderPsid],
-                    webhookEvent,
-                    false
-                  );
-                });
-            } else {
-              setDefaultUser(senderPsid);
-              return receiveAndReturn(users[senderPsid], webhookEvent, false);
-            }
-          } else {
-            i18n.setLocale(users[senderPsid].locale);
-            console.log(
-              "Profile already exists PSID:",
-              senderPsid,
-              "with locale:",
-              i18n.getLocale()
-            );
-            return receiveAndReturn(users[senderPsid], webhookEvent, false);
-          }
-        } else if (user_ref != null && user_ref != undefined) {
-          // Handle user_ref
-          setDefaultUser(user_ref);
-          return receiveAndReturn(users[user_ref], webhookEvent, true);
-        }
-      });
-    });
-  } else {
-    // Return a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+			// Iterate over webhook events - there may be multiple
+			// entry.messaging.forEach(async function(webhookEvent) {
+		      //   // Discard uninteresting events
+		      //   if ("read" in webhookEvent) {
+		      //     console.log("Got a read event");
+		      //     return;
+		      //   } else if ("delivery" in webhookEvent) {
+		      //     console.log("Got a delivery event");
+		      //     return;
+		      //   } else if (webhookEvent.message && webhookEvent.message.is_echo) {
+		      //     console.log(
+		      //       "Got an echo of our send, mid = " + webhookEvent.message.mid
+		      //     );
+		      //     return;
+		      //   }
+
+		      //   // Get the sender PSID
+		      //   let senderPsid = webhookEvent.sender.id;
+		      //   // Get the user_ref if from Chat plugin logged in user
+		      //   let user_ref = webhookEvent.sender.user_ref;
+		      //   // Check if user is guest from Chat plugin guest user
+		      //   let guestUser = isGuestUser(webhookEvent);
+
+		      //   if (senderPsid != null && senderPsid != undefined) {
+		      //     if (!(senderPsid in users)) {
+		      //       if (!guestUser) {
+		      //         // Make call to UserProfile API only if user is not guest
+		      //         let user = new User(senderPsid);
+		      //         GraphApi.getUserProfile(senderPsid)
+		      //           .then(userProfile => {
+		      //             user.setProfile(userProfile);
+		      //           })
+		      //           .catch(error => {
+		      //             // The profile is unavailable
+		      //             console.log(JSON.stringify(body));
+		      //             console.log("Profile is unavailable:", error);
+		      //           })
+		      //           .finally(() => {
+		      //             console.log("locale: " + user.locale);
+		      //             users[senderPsid] = user;
+		      //             i18n.setLocale("en_US");
+		      //             console.log(
+		      //               "New Profile PSID:",
+		      //               senderPsid,
+		      //               "with locale:",
+		      //               i18n.getLocale()
+		      //             );
+		      //             return receiveAndReturn(
+		      //               users[senderPsid],
+		      //               webhookEvent,
+		      //               false
+		      //             );
+		      //           });
+		      //       } else {
+		      //         setDefaultUser(senderPsid);
+		      //         return receiveAndReturn(users[senderPsid], webhookEvent, false);
+		      //       }
+		      //     } else {
+		      //       i18n.setLocale(users[senderPsid].locale);
+		      //       console.log(
+		      //         "Profile already exists PSID:",
+		      //         senderPsid,
+		      //         "with locale:",
+		      //         i18n.getLocale()
+		      //       );
+		      //       return receiveAndReturn(users[senderPsid], webhookEvent, false);
+		      //     }
+		      //   } else if (user_ref != null && user_ref != undefined) {
+		      //     // Handle user_ref
+		      //     setDefaultUser(user_ref);
+		      //     return receiveAndReturn(users[user_ref], webhookEvent, true);
+		      //   }
+		      // });
+		});
+
+		// Returns a '200 OK' response to all requests
+		res.status(200).send("EVENT_RECEIVED");
+		
+	} else {
+		// Return a '404 Not Found' if event is not from a page subscription
+		res.sendStatus(404);
+	}
 });
 
 function setDefaultUser(id) {
@@ -3360,8 +3372,9 @@ function check_fb_signature(req, res, buf) {
     var fb_signature = req.headers["x-hub-signature"];
     if (!fb_signature) {
         throw new Error('Signature ver failed.');
-    } else {
-        var sign_splits = signature.split('=');
+    } 
+    else {
+        var sign_splits = fb_signature.split('=');
         var method = sign_splits[0];
         var sign_hash = sign_splits[1];
 
@@ -3373,4 +3386,20 @@ function check_fb_signature(req, res, buf) {
             throw new Error('Signature ver failed.');
         }
     }
+}
+
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  
 }
